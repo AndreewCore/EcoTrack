b6d6a4a5be0b036c81064ae3d71547760412683a
AndreewCore <andreewinquires@gmail.com>
2026-01-13 00:37:18 -0500

    Reestructuración a imágenes y Primer Fondo Inicializado

--- src/controladores/Controller.java
+++ src/controladores/Controller.java
@@ -9,16 +9,36 @@ package controladores;
  * @author Usuario
  */
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class Controller {
 
     @FXML
-    private Label label;
-
+    private ImageView mainSceneView;
+    
+    @FXML
     public void initialize() {
+        System.out.println("MainScene cargada correctamente");
+        
+        // Información del sistema (opcional)
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        System.out.println("Java Version: " + javaVersion);
+        System.out.println("JavaFX Version: " + javafxVersion);
+        
+        // Configuración adicional del ImageView si es necesario
+        configureImageView();
+    }
+    
+    private void configureImageView() {
+        // Asegurar que la imagen mantenga su proporción
+        mainSceneView.setPreserveRatio(true);
+        
+        // Hacer que la imagen sea suave al escalar
+        mainSceneView.setSmooth(true);
+    }
+    
+    public ImageView getMainSceneView() {
+        return mainSceneView;
     }
 }
