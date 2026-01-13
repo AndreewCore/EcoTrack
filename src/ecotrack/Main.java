da86c15e3d845449cf545e4200589b0809e177b6
AndreewCore <andreewinquires@gmail.com>
2026-01-13 01:03:12 -0500

    MainScene funcional con FXML

--- src/ecotrack/Main.java
+++ src/ecotrack/Main.java
@@ -1,29 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ecotrack;
 
-/**
- *
- * @author Paúl Rodríguez
- */
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/vistas/ecotrack.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        Scene scene = new Scene(root);
+        Image icon = new Image("/images/icon.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("EcoTrack");
+        stage.setScene(scene);
+        //stage.setFullScreen(true);
+        stage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
